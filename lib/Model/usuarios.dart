f1dd9a403e1f5ee8e9d800f1ce6af083cb183ce6
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-03-07 17:30:03 -0300

    Aula 178

--- lib/Model/usuarios.dart
+++ lib/Model/usuarios.dart
@@ -2,22 +2,30 @@ class Usuarios {
   String? _nome;
   String? _email;
   String? _senha;
+  String? _urlImagem;
 
   Usuarios();
 
   Map<String, dynamic> toMap(){
     Map<String, dynamic> map = {
       'nome': this.nome,
-      'email':this.email
+      'email':this.email,
     };
 
     return map;
   }
 
+
   String get senha => _senha!;
 
   set senha(String value) {
     _senha = value;
+  }
+
+  String get urlImagem => _urlImagem!;
+
+  set urlImagem(String value) {
+    _urlImagem = value;
   }
 
   String get email => _email!;
--- lib/telas/AbaContatos.dart
+++ lib/telas/AbaContatos.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../Model/Conversas.dart';
+import '../Model/usuarios.dart';
 
 class AbaContatos extends StatefulWidget {
   const AbaContatos({Key? key}) : super(key: key);
@@ -11,34 +13,90 @@ class AbaContatos extends StatefulWidget {
 
 class _AbaContatosState extends State<AbaContatos> {
 
-  List<Conversa> listaConversas = [
-    Conversa('Ana Clara', 'Olá tudo bem?', 'https://firebasestorage.googleapis.com/v0/b/whatsapp-53c06.appspot.com/o/perfil%2Fperfil1.jpg?alt=media&token=bd073ee5-0112-4c99-8fd9-83d28e4efec4'),
-    Conversa('Pedro Silva', 'Me manda o nome daquela série', 'https://firebasestorage.googleapis.com/v0/b/whatsapp-53c06.appspot.com/o/perfil%2Fperfil2.jpg?alt=media&token=597e96a0-6709-4d4d-894e-60579a339fa4'),
-    Conversa('Marcela Almeida', 'Vamos sair hoje?', 'https://firebasestorage.googleapis.com/v0/b/whatsapp-53c06.appspot.com/o/perfil%2Fperfil3.jpg?alt=media&token=f944a03d-fe6f-4c30-a9a0-936c1d163f37'),
-    Conversa('José Renato', 'Não vai acreditar no que tenho para te contar...', 'https://firebasestorage.googleapis.com/v0/b/whatsapp-53c06.appspot.com/o/perfil%2Fperfil4.jpg?alt=media&token=f91eaff0-f8cb-475e-8ec3-6711616f7f43'),
-    Conversa('Jamilton Damasceno', 'Curso novo!!', 'https://firebasestorage.googleapis.com/v0/b/whatsapp-53c06.appspot.com/o/perfil%2Fperfil5.jpg?alt=media&token=25c83b65-44b6-4b48-bca2-27fce8f0d23b'),
-  ];
+  String? _idusuarioLogado;
+  String? _emailusuarioLogado;
+
+  Future<List<Usuarios>> _recuperarContatos() async {
+    Firestore db = Firestore.instance;
+    QuerySnapshot querySnapshot = await db.collection('usuarios').getDocuments();
+
+    List<Usuarios> listaUsuarios = [];
+    for (DocumentSnapshot item in querySnapshot.documents){
+
+      var dados = item.data;
+
+      if(dados['email']== _emailusuarioLogado) continue;
+
+      Usuarios usuario = Usuarios();
+      usuario.email = dados['email'];
+      usuario.nome = dados['nome'];
+      usuario.urlImagem = dados['urlImagem'];
+
+      listaUsuarios.add(usuario);
+    }
+
+    return listaUsuarios;
+  }
+
+  _recuperarDadosUsuario() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    Firestore db = Firestore.instance;
+    FirebaseUser usuarioLogado = await auth.currentUser();
+    _idusuarioLogado = usuarioLogado.uid;
+    _emailusuarioLogado = usuarioLogado.email;
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _recuperarDadosUsuario();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: listaConversas.length,
-      itemBuilder: (context, index){
-        Conversa conversa = listaConversas[index];
+    return FutureBuilder<List<Usuarios>>(
+      future: _recuperarContatos(),
+        builder: (context, snapshot){
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Carregando contatos...'),
+                    SizedBox(height: 20),
+                    CircularProgressIndicator()
+                  ],
+                ),
+              );
+              break;
+            case ConnectionState.active:
+            case ConnectionState.done:
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (_, index){
 
-        return ListTile(
-          contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
-          leading: CircleAvatar(
-            maxRadius: 30,
-            backgroundColor: Colors.grey,
-            backgroundImage: NetworkImage(conversa.caminhoFoto),
-          ),
-          title: Text(
-            conversa.nome,
-            style: TextStyle( fontWeight: FontWeight.bold, fontSize: 16),
-          ),
-        );
-      },
+                  List<Usuarios> listaItens = snapshot.data!;
+                  Usuarios usuario = listaItens[index];
+
+                  return ListTile(
+                    contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+                    leading: CircleAvatar(
+                      maxRadius: 30,
+                      backgroundColor: Colors.grey,
+                    ),
+                    title: Text(
+                      usuario.nome,
+                      style: TextStyle( fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                  );
+                },
+              );
+              break;
+          }
+        }
     );
   }
 }
